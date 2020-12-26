c26aa55775df95788c63c42f261e69dff008cf3f
bengisuuk <bengisuukarakilinc@gmail.com>
2020-12-26 21:03:30 +0300

    Initial Commit

---
+++ PlacesBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlacesBook
+//
+//  Created by Bengisu Karakılınç on 26.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
